6c57d2d98ca082a3e7c2f7677e7e00e4ca9491f8
Eric <noneya.buisness>
2012-09-25 04:40:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cascade::Application.config.secret_token = '6b4f26a3183a7f2a2a0065c1545b39bfefcefb0798d4c543a23df5a16238ddd6596d430439f8ed092acf4eb34859aca574a7865ba2cf47263ce0f99bbb3d5d5b'
